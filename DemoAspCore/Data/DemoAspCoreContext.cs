9fd9f98050ee4f47e251b9848ea1a1ae07ba7934
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2018-11-12 12:35:20 -0600

    Creacion de Modelo Categorias y controlador

---
+++ DemoAspCore/Data/DemoAspCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoAspCore.Models
+{
+    public class DemoAspCoreContext : DbContext
+    {
+        public DemoAspCoreContext (DbContextOptions<DemoAspCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DemoAspCore.Models.Categoria> Categoria { get; set; }
+    }
+}
---
+++ DemoAspCore/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAspCore.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        [Required (ErrorMessage ="Campo Requerido")]
+        [StringLength(60,MinimumLength =3,ErrorMessage ="El Campo Debe Tener De 3 a 60 Caracteres")]
+        public string Nombre { get; set; }
+        [StringLength(256, ErrorMessage = "La Descripcion No Debe Ser Extensa")]
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+        public Boolean Estado { get; set; }
+    }
+}
