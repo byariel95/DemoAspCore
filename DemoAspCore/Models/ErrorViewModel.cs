cbdffa3ce64db8d799c685653f2dd4e37cfbf7fe
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2018-11-12 10:55:20 -0600

    asp net core template

---
+++ DemoAspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoAspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
